72b5754f84670414ebef24ac62d940853caee38b
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-05-11 01:38:31 -0300

    Display value state, clear, array values

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,25 +4,52 @@ import './Calculator.css';
 import Button from '../components/Button';
 import Display from '../components/Display';
 
+const initialState =  {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculator extends Component {
+
+  state = { ...initialState } // create a initialState clone
+
   constructor(props) {
     super();
     this.addDigit = this.addDigit.bind(this); // 1
   }
   clearMemory() {
-    console.log('clear');
+    this.setState({ ...initialState })
   }
   setOperation(operation) {
     console.log(operation);
   }
   addDigit(n) {
-    console.log(n);
+    if (n === '.' && this.state.displayValue.includes('.')){
+      return;
+    }
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay;
+    const currentValue = clearDisplay ? '' : this.state.displayValue; 
+    const newdisplayValue = currentValue + n;
+    this.setState({ displayValue: newdisplayValue, clearDisplay: false });
+
+    if (n !== '.'){
+      const i = this.state.current;
+      const newValue = parseFloat(newdisplayValue);
+      const values = [...this.state.values];
+      values[i] = newValue; // array item
+      this.setState({ values });
+    }
+
   }
   render() {
     const setOperationConst = op => this.setOperation(op); /* 2 */
     return (
       <div className="calculator">
-        <Display value={100}/>
+        <Display value={this.state.displayValue}/>
         <Button label='AC' click={() => this.clearMemory() /* 3 */} triple />
         <Button label='/' click={setOperationConst /* 2 */} operation /> 
         <Button label='7' click={this.addDigit /* 1 */}/>
